bc18e517bad484b1c53de94cf287f7e8f1bdf274
HebertRyann <hebertryann40@gmail.com>
2021-06-29 15:36:39 -0300

    Updates

---
+++ precache-manifest.a443b558dfd8593a1a64e6d982d57c40.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387d0830841451412b677df8a7e51c4c",
+    "url": "/gobarberr/index.html"
+  },
+  {
+    "revision": "5b3a7dd9b684b2aab3df",
+    "url": "/gobarberr/static/css/2.455cd81f.chunk.css"
+  },
+  {
+    "revision": "5b3a7dd9b684b2aab3df",
+    "url": "/gobarberr/static/js/2.83a05a1e.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/gobarberr/static/js/2.83a05a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a0de89dbd239fe2bc8",
+    "url": "/gobarberr/static/js/main.2c9f1a8b.chunk.js"
+  },
+  {
+    "revision": "7285eb5e58bcac150835",
+    "url": "/gobarberr/static/js/runtime-main.b19850b2.js"
+  },
+  {
+    "revision": "cb3c3e0d86c1fc98cdf6b403ff07ff76",
+    "url": "/gobarberr/static/media/logo.cb3c3e0d.svg"
+  },
+  {
+    "revision": "6eac314f78fe224c4d769fb646523773",
+    "url": "/gobarberr/static/media/sign-in-background.6eac314f.png"
+  },
+  {
+    "revision": "c4d8f988b9c6499d56702a398b2f3d8b",
+    "url": "/gobarberr/static/media/sign-up-background.c4d8f988.png"
+  }
+]);
